029acbdeb134a799cc93d715968b561847411e4b
ATMIRA\andreu.conesa <andreuconesasimo@gmail.com>
2018-05-07 10:11:19 +0200

    modified index.cshtml to provide a better view, added UnityConfig class to use Unity in dependency injection

---
+++ WebApi/App_Start/UnityConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using Unity;
+using Unity.Lifetime;
+using WebApi.Business.Logic;
+using WebApi.DataAccess.Dao;
+using WebApi.DataAccess.Dao.Interfaces;
+
+namespace WebApi
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IStudentBL, StudentBL>(new HierarchicalLifetimeManager());
+            container.RegisterType<IFileStudent, StudentJsonFile>(new HierarchicalLifetimeManager());
+            container.RegisterType<IFileStudent, StudentSPFile>(new HierarchicalLifetimeManager());
+            container.RegisterType<IFileStudent, StudentSqlFile>(new HierarchicalLifetimeManager());
+            container.RegisterType<IFileStudent, StudentTxtFile>(new HierarchicalLifetimeManager());
+            container.RegisterType<IFileStudent, StudentXmlFile>(new HierarchicalLifetimeManager());
+
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityResolver(container);
+        }
+    }
+}
--- WebApi/App_Start/WebApiConfig.cs
+++ WebApi/App_Start/WebApiConfig.cs
@@ -1,9 +1,4 @@
 ﻿using System.Web.Http;
-using Unity;
-using Unity.Lifetime;
-using WebApi.Business.Logic;
-using WebApi.DataAccess.Dao;
-using WebApi.DataAccess.Dao.Interfaces;
 
 namespace WebApi
 {
@@ -21,15 +16,6 @@ namespace WebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
-            var container = new UnityContainer();
-            container.RegisterType<IStudentBL, StudentBL>(new HierarchicalLifetimeManager());
-            container.RegisterType<IFileStudent, StudentJsonFile>(new HierarchicalLifetimeManager());
-            container.RegisterType<IFileStudent, StudentSPFile>(new HierarchicalLifetimeManager());
-            container.RegisterType<IFileStudent, StudentSqlFile>(new HierarchicalLifetimeManager());
-            container.RegisterType<IFileStudent, StudentTxtFile>(new HierarchicalLifetimeManager());
-            container.RegisterType<IFileStudent, StudentXmlFile>(new HierarchicalLifetimeManager());
-            config.DependencyResolver = new UnityResolver(container);
         }
     }
 }
